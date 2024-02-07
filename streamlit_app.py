ccfae5b3018f311f4030f98f8a182702aace7803
Firas Nohra <149088244+Firasn0@users.noreply.github.com>
2024-02-07 09:12:15 +0100

    Create streamlit_app.py
    this is my demo file

---
+++ streamlit_app.py
@@ -0,0 +1,13 @@
+import streamlit as sl
+
+sl.title("Streamlit tutorial")
+sl.markdown("""
+<style>
+    .st-emotion-cache-zq5wmm.ezrtsby0
+    {
+        visibility: hidden;
+    }
+</style>
+""", unsafe_allow_html=True)
+
+sl.header("Hello Nikola")
